52f2e417327a21fc9ee803952873439024197dbf
nashsclay <nashsclay@gmail.com>
2018-06-24 08:45:20 -0700

    Update wallet_create.sh

--- wallet_create.sh
+++ wallet_create.sh
@@ -9,8 +9,10 @@ sudo add-apt-repository ppa:bitcoin/bitcoin
 sudo apt-get update
 sudo apt-get install libdb4.8-dev libdb4.8++-dev -y
 cd
-sudo git clone https://github.com/zero24x/flexinodes
-cd flexinodes 
+wget https://github.com/zero24x/flexinodes/archive/untagged-74609a6f600f7eb88ba5.tar.gz
+tar -xzf flexinodes-untagged-74609a6f600f7eb88ba5.tar.gz
+#sudo git clone https://github.com/zero24x/flexinodes
+cd flexinodes-untagged-74609a6f600f7eb88ba5 
 sudo ./autogen.sh
 sudo ./configure
 sudo make    #Delete -j4 if don't know how many cores (CPU) Same for next line.
