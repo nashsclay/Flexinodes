1d14a2acc609e5b2a859c0e538e6b21d9d2f2960
nashsclay <nashsclay@gmail.com>
2018-06-24 19:27:39 -0700

    Update update_flexinodes_mn.sh

--- update_flexinodes_mn.sh
+++ update_flexinodes_mn.sh
@@ -8,13 +8,8 @@ echo
 
 cd ~
 
-echo "! Removing Flexinodes !"
-sudo rm -f /usr/local/bin/flexinodesd
-sudo rm -f /usr/local/bin/flexinodes-cli
-sudo rm -f /usr/local/bin/flexinodes-qt
-
 wget https://github.com/nashsclay/Flexinodes/releases/download/untagged-89d6a4401e5d3ca9d34e/flexinodes_simple_1.0.0.5.tar.gz
-sudo tar -xf flexinodes_simple_1.0.0.5.tar.gz /usr/local/bin
+sudo tar -xvf --overwrite flexinodes_simple_1.0.0.5.tar.gz /usr/local/bin
 sudo rm flexinodes_simple_1.0.0.5.tar.gz
 echo "! Restarting Flexinodes Masternode !"
 sudo flexinodes-cli stop
