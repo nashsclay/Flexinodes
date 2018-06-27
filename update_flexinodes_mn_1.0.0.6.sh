705c54c1c153daa055cefd4eaa0b64b552fef6c0
nashsclay <nashsclay@gmail.com>
2018-06-27 09:10:46 -0700

    Update update_flexinodes_mn_1.0.0.6.sh

--- update_flexinodes_mn_1.0.0.6.sh
+++ update_flexinodes_mn_1.0.0.6.sh
@@ -8,10 +8,10 @@ echo
 
 cd ~
 
-wget https://github.com/nashsclay/Flexinodes/releases/download/untagged-89d6a4401e5d3ca9d34e/flexinodes_simple_1.0.0.5.tar.gz
-sudo tar -xvf flexinodes_simple_1.0.0.5.tar.gz
+wget https://github.com/nashsclay/Flexinodes/releases/download/untagged-a6ca1662dfd508ffbc62/flexinodes_updated.1.0.0.6.tar.gz
+sudo tar -xvf flexinodes_updated.1.0.0.6.tar.gz
 sudo mv -f ./flexinodes-cli /usr/local/bin
 sudo mv -f ./flexinodesd /usr/local/bin
-sudo rm flexinodes_simple_1.0.0.5.tar.gz
+sudo rm flexinodes_updated.1.0.0.6.tar.gz
 echo "! Restarting Flexinodes Masternode !"
 sudo flexinodes-cli stop
