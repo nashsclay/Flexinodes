6e19c52af991af5797af512342a8f761f80d6f5d
nashsclay <nashsclay@gmail.com>
2018-06-29 03:46:16 -0700

    Update update_flexinodes_mn_1.0.0.7.sh

--- update_flexinodes_mn_1.0.0.7.sh
+++ update_flexinodes_mn_1.0.0.7.sh
@@ -8,7 +8,7 @@ echo
 
 cd ~
 
-wget https://github.com/nashsclay/Flexinodes/releases/download/untagged-a6ca1662dfd508ffbc62/flexinodes_updated.1.0.0.6.tar.gz
+wget https://github.com/nashsclay/Flexinodes/releases/download/untagged-1d2b11f03d899d08d562/flexinodes_1007update.tar.gz
 sudo tar -xvf flexinodes_updated.1.0.0.6.tar.gz
 sudo mv -f ./flexinodes-cli /usr/local/bin
 sudo mv -f ./flexinodesd /usr/local/bin
